7fea6f188d0ba59692a99862ce71fe3f12d17072
maricielo <cielotapia198@gmail.com>
2025-06-14 00:16:50 -0500

    Ejercicio 3 – avance 1: Entrada de datos para catetos

---
+++ Ejercicio3.cpp
@@ -0,0 +1,22 @@
+/*Cálculo de la Hipotenusa: Crea un programa que calcule la hipotenusa de un triángulo rectángulo
+utilizando la fórmula de Pitágoras. Solicita al usuario los valores de los catetos y utiliza la función
+sqrt de cmath para calcular la raíz cuadrada de la suma de los cuadrados de los catetos. Muestra el
+resultado de la hipotenusa calculada.*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main (){
+	double Cateto1, Cateto2, Hipotenusa;
+	
+	cout << "Ingrese el primer cateto de su triangulo" << endl;
+	cin >> Cateto1;
+	
+	cout << "Ingrese el segundo cateto de su triangulo" << endl;
+	cin >> Cateto2;
+	
+	
+	return 0;
+}
