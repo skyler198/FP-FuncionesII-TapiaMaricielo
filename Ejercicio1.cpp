6dd3b9a61ae0d9a1590293ef006e57c2448ce387
maricielo <cielotapia198@gmail.com>
2025-06-13 21:55:13 -0500

    Ejercicio 1 – avance 1: Estructura base

---
+++ Ejercicio1.cpp
@@ -0,0 +1,21 @@
+/*Cálculo del Área de un Círculo: Implementa un programa que calcule el área de un círculo dado su
+radio. Utiliza la constante M_PI de la librería cmath para obtener el valor de pi. Solicita al usuario el
+radio del círculo y utiliza la función pow de cmath para calcular el cuadrado del radio. Muestra el
+resultado del área calculada.*/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+double CalcularAreaCircular(double radio){
+	
+}
+
+int main(){
+	double radio, Area;
+	
+	cout << "ingrese el radio del circulo" << endl;
+	cin >> radio;
+	
+	return 0;
+}
