dde9860180be91f6a5187ef2bdfae7a8cb323f37
maricielo <cielotapia198@gmail.com>
2025-06-15 04:30:26 -0500

    Ejercicio 8 - Simulación de Tragamonedas Simple: Se agregó bucle de repetición (do-while), corrección en función de escalera y validación completa de apuestas, versión final

--- Ejercicio8.cpp
+++ Ejercicio8.cpp
@@ -46,60 +46,79 @@ int minimo(int a, int b, int c) {
 
  
 bool esEscalera(int a, int b, int c) {
-    if (a != b && a != c && b != c) {
-        int maxNum = maximo(a, b, c);
-        int minNum = minimo(a, b, c);
-        return (maxNum - minNum == 2 && (a + b + c == minNum + minNum + 1 + maxNum));
+    // Escalera ascendente 
+    if ((a == 1 && b == 2 && c == 3) || 
+        (a == 2 && b == 3 && c == 4) ||
+        (a == 3 && b == 4 && c == 5)) {
+        return true;
     }
+
+    // Escalera descendente 
+    if ((a == 3 && b == 2 && c == 1) || 
+        (a == 4 && b == 3 && c == 2) ||
+        (a == 5 && b == 4 && c == 3)) {
+        return true;
+    }
+
     return false;
 }
+
 
 int main() {
     srand(time(0));
     
     int monedas = 100;
     int apuesta, numero1, numero2, numero3;
+    char respuesta;
 
     cout << "==========================================================" << endl;
     cout << "   BIENVENIDO A LA MAQUINA TRAGAMONEDAS VIRTUAL DE SKY!" << endl;
     cout << "==========================================================" << endl;
-
-    cout << "\nTienes " << monedas << " monedas disponibles." << endl;
-    cout << "Cuantas monedas deseas apostar? (1 a 10): ";
-    cin >> apuesta;
-
-    while (apuesta < 1 || apuesta > 10 || apuesta > monedas) {
-        cout << "Apuesta invalida, Intenta nuevamente: ";
-        cin >> apuesta;
-    }
-
-  
-    numero1 = generarNumero();
-    numero2 = generarNumero();
-    numero3 = generarNumero();
-
-    cout << "\nSus numeros aleatorios son: \n";
-    mostrarNumeros(numero1, numero2, numero3);
     
+    do{
+    	cout << "\nTienes " << monedas << " monedas disponibles." << endl;
+	    cout << "Cuantas monedas deseas apostar? (1 a 10): ";
+	    cin >> apuesta;
+	
+	    while (apuesta < 1 || apuesta > 10 || apuesta > monedas) {
+	        cout << "Apuesta invalida, Intenta nuevamente: ";
+	        cin >> apuesta;
+	    }
+	
+	  
+	    numero1 = generarNumero();
+	    numero2 = generarNumero();
+	    numero3 = generarNumero();
+	
+	    cout << "\nSus numeros aleatorios son: \n";
+	    mostrarNumeros(numero1, numero2, numero3);
+	    
+	    
+		 if (tresIguales(numero1, numero2, numero3)) {
+	        cout << "\nFelicidades! GANASTE 10 veces tu apuesta!" << endl;
+	        monedas += apuesta * 9; // recupera su apuesta + gana 9 veces
+	    } else if (esEscalera(numero1, numero2, numero3)) {
+	        cout << "\nWow! Formaste una escalera! GANASTE 5 veces tu apuesta!" << endl;
+	        monedas += apuesta * 4;
+	    } else if (dosIguales(numero1, numero2, numero3)) {
+	        cout << "\nBien! Hay dos iguales, GANASTE el doble de tu apuesta!" << endl;
+	        monedas += apuesta;
+	    } else {
+	        cout << "\nLo siento! Perdiste tu apuesta" << endl;
+	        monedas -= apuesta;
+	    }
+	
+	    if (monedas <= 0) {
+	        cout << "\nTe has quedado sin monedas. JUEGO TERMINADO." << endl;
+	        break;
+	    }
+	    cout << "\nTe quedan " << monedas << " monedas." << endl;
+	
+        cout << "\nDeseas jugar otra vez? (s/n): ";
+        cin >> respuesta;
+        
+	}while (respuesta == 's' || respuesta == 'S');
     
-	 if (tresIguales(numero1, numero2, numero3)) {
-        cout << "\nFelicidades! GANASTE 10 veces tu apuesta!" << endl;
-        monedas += apuesta * 9; // recupera su apuesta + gana 9 veces
-    } else if (esEscalera(numero1, numero2, numero3)) {
-        cout << "\nWow! Formaste una escalera! GANASTE 5 veces tu apuesta!" << endl;
-        monedas += apuesta * 4;
-    } else if (dosIguales(numero1, numero2, numero3)) {
-        cout << "\nBien! Hay dos iguales, GANASTE el doble de tu apuesta!" << endl;
-        monedas += apuesta;
-    } else {
-        cout << "\nLo siento! Perdiste tu apuesta" << endl;
-        monedas -= apuesta;
-    }
-
-    if (monedas <= 0) {
-        cout << "\nTe has quedado sin monedas. JUEGO TERMINADO." << endl;
-    }
-    cout << "\nTe quedan " << monedas << " monedas." << endl;
-    
+    cout << "\nGracias por jugar. Vuelve pronto!" << endl;
     return 0;
 }
